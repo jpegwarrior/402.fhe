2c471dde61b10279fdfd8f282558a3c2202f1a70
Aditya Mane <aadityamane765@gmail.com>
2026-02-23 21:14:27 +0530

    added missing chainid

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -17,6 +17,7 @@ const config: HardhatUserConfig = {
     },
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
+      chainId: 11155111,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
